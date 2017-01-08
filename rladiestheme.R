ef0414d8e60bb17b357637a996bd27cfb732d6eb
Maëlle Salmon <maelle.salmon@yahoo.se>
2017-01-08 09:59:48 +0100

    first commit

---
+++ rladiestheme.R
@@ -0,0 +1,13 @@
+# R-Ladies ggplot2 theme
+
+# main purple color: #88398A
+
+# grey color: #D3D3D3
+
+# black color: #181818
+
+# white color: #FFFFFF
+
+# dark purple color: #562457
+
+# font Helvetica Neue
